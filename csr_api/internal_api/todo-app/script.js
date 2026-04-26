2c554a21192086a24c2c2e7d92304c8c8b905fac
xlit.online <sonukumar_x_lit@outlook.com>
2026-04-26 16:11:18 +0530

    Renamed files

--- csr_api/internal_api/todo-app/script.js
+++ csr_api/internal_api/todo-app/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api/todos";
+const API_URL = "import.meta.env.VITE_API_URL";
 
 const todoForm = document.getElementById("todoForm");
 const todoInput = document.getElementById("todoInput");
