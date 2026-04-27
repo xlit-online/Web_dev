f653cb71a711e47609496fb7e8a2a6aca3a513f3
xlit.online <sonukumar_x_lit@outlook.com>
2026-04-27 20:06:09 +0530

    small fix

--- csr_api/internal_api/routes/todo.route.js
+++ csr_api/internal_api/routes/todo.route.js
@@ -1,24 +1,29 @@
 const todoController = require("../controllers/todo.controller");
 
 function todoRoutes(req, res) {
-  const url = req.url; // already cleaned (pathname only)
+  const url = req.pathname; // 👈 from router.js
+  const method = req.method;
 
-  if (url === "/api/todos" && req.method === "GET") {
+  // GET all todos
+  if (url === "/api/todos" && method === "GET") {
     todoController.getTodos(req, res);
     return true;
   }
 
-  if (url === "/api/todos" && req.method === "POST") {
+  // POST todo
+  if (url === "/api/todos" && method === "POST") {
     todoController.addTodo(req, res);
     return true;
   }
 
-  if (url === "/api/todos" && req.method === "PUT") {
+  // PUT todo
+  if (url === "/api/todos" && method === "PUT") {
     todoController.updateTodo(req, res);
     return true;
   }
 
-  if (url === "/api/todos" && req.method === "DELETE") {
+  // DELETE todo
+  if (url === "/api/todos" && method === "DELETE") {
     todoController.deleteTodo(req, res);
     return true;
   }
