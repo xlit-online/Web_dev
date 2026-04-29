84552e8a3a4335834a2daeb411bb303d204013f7
xlit.online <sonukumar_x_lit@outlook.com>
2026-04-29 17:17:00 +0530

    Fixing syntax error

--- csr_api/internal_api/models/Note.js
+++ csr_api/internal_api/models/Note.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const noteSchema = new mongoose.Schema({
+const noteSchema = new mongoose.Schema(
     {
         title: { type: String, required: true },
         content: { type: String, required: true}
