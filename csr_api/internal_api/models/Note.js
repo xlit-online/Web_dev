53dc3c505b8453d3656a754133719a8e1585d704
xlit.online <sonukumar_x_lit@outlook.com>
2026-04-29 17:21:51 +0530

    Fixing missing , as syntax error

--- csr_api/internal_api/models/Note.js
+++ csr_api/internal_api/models/Note.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const noteSchema = new mongoose.Schema(
-    {
-        title: { type: String, required: true },
-        content: { type: String, required: true}
-        tags: { type: [String], default: []}
-    }, 
-    { timestamps: true }
+  {
+    title: { type: String, required: true, trim: true },
+    content: { type: String, required: true, trim: true },
+    tags: { type: [String], default: [] },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Note", noteSchema);
