b6bb69d16886d4086e9ff28eb5364a6eae4903b9
xlit.online <sonukumar_x_lit@outlook.com>
2026-05-02 14:23:26 +0530

    Fixing error

--- Projects/csr_api/internal_api/routes/link.route.js
+++ Projects/csr_api/internal_api/routes/link.route.js
@@ -1,34 +1,26 @@
-const {
-  shortenUrl,
-  getAllLinks,
-  getLinkAnalytics,
-  redirectToOriginal,
-} = require("../controllers/link.controller");
-
 function linkRoutes(req, res) {
-  // POST /api/shorten
-  if (req.url === "/api/shorten" && req.method === "POST") {
+  const pathname = req.parsedUrl.pathname;
+
+  if (pathname === "/api/shorten" && req.method === "POST") {
     shortenUrl(req, res);
     return true;
   }
 
-  // GET /api/links
-  if (req.url === "/api/links" && req.method === "GET") {
+  if (pathname === "/api/links" && req.method === "GET") {
     getAllLinks(req, res);
     return true;
   }
 
-  // GET /api/links/:code
-  if (req.url.startsWith("/api/links/") && req.method === "GET") {
-    const code = req.url.split("/")[3];
+  if (pathname.startsWith("/api/links/") && req.method === "GET") {
+    const code = pathname.split("/")[3];
     getLinkAnalytics(req, res, code);
     return true;
   }
 
-  // GET /:code (redirect)
-  if (!req.url.startsWith("/api") && req.method === "GET") {
-    const code = req.url.substring(1);
-    if (code.length > 0) {
+  // redirect
+  if (!pathname.startsWith("/api") && req.method === "GET") {
+    const code = pathname.substring(1);
+    if (code) {
       redirectToOriginal(req, res, code);
       return true;
     }
@@ -36,5 +28,3 @@ function linkRoutes(req, res) {
 
   return false;
 }
-
-module.exports = linkRoutes;
